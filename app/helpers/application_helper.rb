a534bf62c2552fa0c645e7b20c2dff9dc2a0e5bb
Seyed Razavi <seyed.razavi@thelearningclinic.co.uk>
2016-04-18 20:23:25 +0100

    Fix GA helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -133,16 +133,14 @@ module ApplicationHelper
 
   def google_analytics
     unless this_blog.google_analytics.empty?
-      <<-HTML
-      <script type="text/javascript">
-      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+"      <script type="text/javascript">
+      var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");
+      document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));
       </script>
-      <script type="text/javascript">
-      var pageTracker = _gat._getTracker("#{this_blog.google_analytics}");
+      <script type=\"text/javascript\">
+      var pageTracker = _gat._getTracker(\"#{this_blog.google_analytics}\");
       pageTracker._trackPageview();
-      </script>
-      HTML
+      </script>".html_safe
     end
   end
 
